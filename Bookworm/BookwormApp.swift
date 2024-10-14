db9000af7ee92dcef5837d29b178d05ddcb063cf
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-14 11:40:38 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Juan Carlos Robledo Morales on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookwormTests/BookwormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormTests.swift
+//  BookwormTests
+//
+//  Created by Juan Carlos Robledo Morales on 14/10/24.
+//
+
+import Testing
+@testable import Bookworm
+
+struct BookwormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
