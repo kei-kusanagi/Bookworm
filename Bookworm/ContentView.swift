e95c5f6255f2458c1ca3e67f3e19bf4bfa56dfd8
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-14 17:37:08 -0600

    Accepting multi-line text input with TextEditor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,35 +7,21 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title){
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: isOn ? onColors: offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 
 struct ContentView: View {
-    @State private var rememberMe = false
-    
+    @AppStorage("notes") private var notes = ""
+
     var body: some View {
-        VStack{
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off" )
+        NavigationStack {
+            TextField("Enter your text", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .navigationTitle("Notes")
+                .padding()
         }
     }
 }
+
+
 
 #Preview {
     ContentView()
