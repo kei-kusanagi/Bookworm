f1b734a3e361b5ad1b60bd5a610a1b448b4a9607
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-10-15 13:01:15 -0600

    Creating books with SwiftData

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -13,6 +13,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Student.self) 
+        .modelContainer(for: Book.self)
     }
 }
